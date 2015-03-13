a6ac19b166eae3b7f67f8dcca85ce0154885b4d6
Diaconu Stefan <sm_echer@yahoo.com>
2015-03-13 15:42:41 +0200

    Initial check-in

---
+++ MyFirstGitProject/src/pachet1/Apples.java
@@ -0,0 +1,11 @@
+package pachet1;
+
+public class Apples {
+
+	public static void main(String[] args) {
+			
+		System.out.println("Apples page");
+		System.out.println("apple");
+	}
+
+}
---
+++ MyFirstGitProject/src/pachet1/Banana.java
@@ -0,0 +1,10 @@
+package pachet1;
+
+public class Banana {
+
+	public static void main (String argv[])
+	{
+		System.out.println("Banana");
+	}
+	
+}
